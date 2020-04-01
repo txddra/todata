14d57478b08fe9841cb3cd4b9134484a2ebbb44a
Your Name <you@example.com>
2020-04-01 14:32:49 -0400

    defeated

---
+++ add-todos-button-controller.js
@@ -0,0 +1,6 @@
+document.querySelector('.add-todo').addEventListener('click', function(){
+    const text = document.querySelector('.todo-input').value
+    const priority = document.querySelector('.priority').value
+    const complete = false
+    const id = todos.length
+} 
