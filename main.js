ae22b6be73250e0882694e9c2714645e486f9ca8
Your Name <you@example.com>
2020-04-01 14:47:50 -0400

    ??

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
-document.querySelector()
+document.querySelector(".add-todo").addEventListener("click", function() {
+	let textInput = document.querySelector(".todo-input");
+	let priority = document.querySelector(".priority");
+const newEntry = {
+		text: textInput.value,
+		id: todos.length,
+		complete: false,
+		priority: Number(priority.value)
+	};
+
+	addTodo(newEntry);
+	textInput.value = "";
+	priority.value = 1;
+
+	refreshTodos();
+});
