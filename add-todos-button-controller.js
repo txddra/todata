614b6f16837d63a11c977cbc694663ca2e76f9dc
Your Name <you@example.com>
2020-04-01 16:21:03 -0400

    help wilson, you're my only help

--- add-todos-button-controller.js
+++ add-todos-button-controller.js
@@ -1,6 +1,18 @@
 document.querySelector('.add-todo').addEventListener('click', function(){
-    const text = document.querySelector('.todo-input').value
-    const priority = document.querySelector('.priority').value
-    const complete = false
-    const id = todos.length
-} 
+    const text = document.querySelector('.todo-input').value,
+    const priority = document.querySelector('.priority').value,
+    const complete = false,
+    const id = todos.length,
+
+
+    const newObj = {
+text,
+priority,
+complete,
+id,
+
+}
+addTodo(newObj)
+printTodo(newObj)
+
+})
